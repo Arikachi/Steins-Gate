03171e5aedaa60d97c8e4b00506968537d85989d
Minh Trí <88954074+Arikachi@users.noreply.github.com>
2022-06-10 16:38:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const container = document.querySelector('.container')
 container.addEventListener('click', () => {
     mp3.play()
 });
+
+container.addEventListener('ontouchstart', () => {
+    mp3.play()
+});
